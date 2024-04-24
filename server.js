e4da7711c656f35913c98bd0ea2aaa18714ceaf6
bliss <107047190+Blisse1@users.noreply.github.com>
2024-04-24 12:51:33 -0500

    delete some things

--- server.js
+++ server.js
@@ -1,15 +1,4 @@
 import fs from "fs";
 import OpenAI from "openai";
 import "dotenv/config"
-
-const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY} );
-async function main() {
-    const transcription = await openai.audio.transcriptions.create({
-        file: fs.createReadStream("/path/to/file"),
-        model: "whisper-1",
-    });
-
-    console.log(transcription.text);
-}
-main();
 breakfast solution
