31e663ddd3994f3b8eaa8a97c42938e3823a5ffa
bliss <107047190+Blisse1@users.noreply.github.com>
2024-04-24 12:15:32 -0500

    Revert "breaking-change"
    This reverts commit 2dadb84129b04ead308a9d641f9313fd91fed4be.

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-breaking-change
-sadlaksj
-sad
-dsa
-sda
-dsa
-dsa
+import fs from "fs";
+import OpenAI from "openai";
+import "dotenv/config"
+
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY} );
+async function main() {
+    const transcription = await openai.audio.transcriptions.create({
+        file: fs.createReadStream("/path/to/file"),
+        model: "whisper-1",
+    });
+
+    console.log(transcription.text);
+}
+main();
+new flow
