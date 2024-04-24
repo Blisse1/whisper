79e12a86abbcee8dcc85eeee75d5627a43509fe0
bliss <107047190+Blisse1@users.noreply.github.com>
2024-04-24 12:58:57 -0500

    original

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 import fs from "fs";
 import OpenAI from "openai";
 import "dotenv/config"
-breakfast solution
+
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY} );
+async function main() {
+    const transcription = await openai.audio.transcriptions.create({
+        file: fs.createReadStream("/path/to/file"),
+        model: "whisper-1",
+    });
+
+    console.log(transcription.text);
+}
+main();
